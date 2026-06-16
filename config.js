10009d85f5e111a8b12eb0c5806831941b01995d
liuxiaotong1 <64093061+liuxiaotong1@users.noreply.github.com>
2026-06-16 10:50:13 +0200

    Add config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window.SITE_CONFIG = {
+  checkoutUrl: "https://buy.stripe.com/28EdR9fcu65l3hfdjZ6oo00",
+  contactEmail: "fulifulibang@gmail.com",
+  previewMode: true,
+  sellerName: "",
+  sellerLegalStatus: "",
+  registrationNumber: "",
+  sellerAddress: "",
+  sellerCountry: "",
+};
