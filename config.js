7bde1e8c936ed50aa81ceb4dcc9b61dbc9b389a6
liuxiaotong1 <64093061+liuxiaotong1@users.noreply.github.com>
2026-06-17 22:09:41 +0200

    Open founding checkout config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.SITE_CONFIG = {
   checkoutUrl: "https://buy.stripe.com/28EdR9fcu65l3hfdjZ6oo00",
   contactEmail: "fulifulibang@gmail.com",
-  previewMode: true,
+  previewMode: false,
   sellerName: "",
   sellerLegalStatus: "",
   registrationNumber: "",
